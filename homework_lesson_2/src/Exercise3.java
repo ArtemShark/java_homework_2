8053e2633b72f2bb499d1f0e03e9cde6ca2c8668
Artem <artem1podderegin@gmail.com>
2024-02-02 11:38:27 +0100

    third exercise

---
+++ homework_lesson_2/src/Exercise3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первую цифру: ");
+        int digit1 = scanner.nextInt();
+
+        System.out.print("Введите вторую цифру: ");
+        int digit2 = scanner.nextInt();
+
+        System.out.print("Введите третью цифру: ");
+        int digit3 = scanner.nextInt();
+
+        int result = digit1 * 100 + digit2 * 10 + digit3;
+        System.out.println("Результат: " + result);
+    }
+}
